b57acf0b01a3202578744689a292fb899e797a86
Alexander Strong <astrong3@binghamton.edu>
2022-11-30 00:21:54 -0500

    changed defensive score algorithm; now considers how many pokemon of the offensive type there are

--- defensive.mjs
+++ defensive.mjs
@@ -9,7 +9,7 @@ Promise.all(util.types.map(get_move_data)).then((moves) => {
     const single_type_graph = util.type_adjlist();
     get_mons.get_mons().then((mons) => {
 	//get the average physical/special atttack of each type
-	//entry format: type {p, s}
+	//entry format: type {p, s, c}
 	const avg_atks = {};
 	for(const type of util.types) {
 	    const _type = (type === "psychict")?("psychic"):(type);
@@ -17,9 +17,9 @@ Promise.all(util.types.map(get_move_data)).then((moves) => {
 	    //console.log(`There are ${mons_of_type.length} Pokemon with the ${_type} type`);
 	    const avg_phys_atk = util.avg(mons_of_type.map(mon => parseInt(mon.att)));
 	    const avg_spec_atk = util.avg(mons_of_type.map(mon => parseInt(mon.satt)));
-	    avg_atks[type] = {p: avg_phys_atk, s: avg_spec_atk};
+	    avg_atks[type] = {p: avg_phys_atk, s: avg_spec_atk, c: mons_of_type.length};
 	}
-	console.log(avg_atks);
+	//console.log(avg_atks);
 	var content = "type,def_score";
 	for(let i = 0; i < util.types.length; i++) {
 	    const ti = util.types[i];
@@ -35,14 +35,14 @@ Promise.all(util.types.map(get_move_data)).then((moves) => {
 			    var def_score = 0;
 			    if(mt === mt2) {
 				const e = single_type_graph[ti][mt.type].def * (i === j)?(1):(single_type_graph[tj][mt.type].def);
-				const def_score_mt = e * (mt.physical * mt.physicalCount * avg_atks[mt.type].p + mt.special * mt.specialCount * avg_atks[mt.type].s) / (mt.physicalCount + mt.specialCount);
+				const def_score_mt = e * (mt.physical * mt.physicalCount * avg_atks[mt.type].p + mt.special * mt.specialCount * avg_atks[mt.type].s) / (mt.physicalCount + mt.specialCount) * avg_atks[mt.type].c/mons.length;
 				def_score = def_score_mt;
 			    }
 			    else {
 				const e = single_type_graph[ti][mt.type].def * (i === j)?(1):(single_type_graph[tj][mt.type].def);
-				const def_score_mt = e * (mt.physical * mt.physicalCount * avg_atks[mt.type].p + mt.special * mt.specialCount * avg_atks[mt.type].s) / (mt.physicalCount + mt.specialCount);
+				const def_score_mt = e * (mt.physical * mt.physicalCount * avg_atks[mt.type].p + mt.special * mt.specialCount * avg_atks[mt.type].s) / (mt.physicalCount + mt.specialCount) * avg_atks[mt.type].c/mons.length;
 				const e2 = single_type_graph[ti][mt2.type].def * (i === j)?(1):(single_type_graph[tj][mt2.type].def);
-				const def_score_mt2 = e2 * (mt2.physical * mt2.physicalCount * avg_atks[mt2.type].p + mt2.special * mt2.specialCount * avg_atks[mt2.type].s) / (mt2.physicalCount + mt2.specialCount);
+				const def_score_mt2 = e2 * (mt2.physical * mt2.physicalCount * avg_atks[mt2.type].p + mt2.special * mt2.specialCount * avg_atks[mt2.type].s) / (mt2.physicalCount + mt2.specialCount) * avg_atks[mt2.type].c/mons.length;
 				def_score = (def_score_mt > def_score_mt2)?(def_score_mt):(def_score_mt2);
 			    }
 			    matchups[mts_as_str] = def_score;
